1c67aab38bc5a914ff24c244b758e93331a2d6e6
Andrew Brandt <abrandt@makeschool.com>
2014-11-22 20:42:22 -0800

    first commit

---
+++ tether WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  tether WatchKit Extension
+//
+//  Created by Andrew Brandt on 11/22/14.
+//  Copyright (c) 2014 dorystudios. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ tether WatchKit Extension/InterfaceController.m
@@ -0,0 +1,47 @@
+//
+//  InterfaceController.m
+//  tether WatchKit Extension
+//
+//  Created by Andrew Brandt on 11/22/14.
+//  Copyright (c) 2014 dorystudios. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (IBAction)notifyPhone {
+    
+}
+
+- (instancetype)initWithContext:(id)context {
+    self = [super initWithContext:context];
+    if (self){
+        // Initialize variables here.
+        // Configure interface objects here.
+        NSLog(@"%@ initWithContext", self);
+        
+    }
+    return self;
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    NSLog(@"%@ will activate", self);
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    NSLog(@"%@ did deactivate", self);
+}
+
+@end
+
+
+
